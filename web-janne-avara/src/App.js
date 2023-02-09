03bef8c8d5a182fb4ac4a656f7291882720924bb
Pedro Bergamo <pedrobergamo89@gmail.com>
2023-02-09 11:21:56 +0200

    Splash Screen

---
+++ web-janne-avara/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import SplashScreen from './components/SplashScreen.js'
+
+function App() {
+  return (
+    <div className="App">
+      <SplashScreen/>
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
