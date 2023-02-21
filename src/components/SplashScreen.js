3765d833cd5f5ba85bcecda65febee7fc6851ff1
Pedro Bergamo <pedrobergamo89@gmail.com>
2023-02-21 10:39:45 +0200

    add video to splash screen

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,6 +1,6 @@
 import "../css/Components.css"
 
-//import SplashVideo from "../resources/splash-video-2.mp4";
+import SplashVideo from "../resources/janne-video.mp4";
 
 import Logo from "../images/janne-logo-white.png";
 
@@ -8,6 +8,11 @@ var Video = "https://www.youtube.com/watch?v=8KsibZ0Hqxk";
 
 const SplashScreen = () =>
   <div className="video-container">
+
+      <video className="splash-video" autoPlay loop muted>
+        <source src={SplashVideo} type="video/mp4"/>
+      </video>
+
     <div className="logo">
       <img src={Logo} width="100vh" alt=""/>
     </div>
@@ -16,16 +21,6 @@ const SplashScreen = () =>
           <a href={Video} target= "_blank" rel="noreferrer">
           <h1>Kuulen puiden versovan</h1></a>
     </div>
-        {/*<video className="splash-video" autoPlay loop muted>
-          <source src={SplashVideo} type="video/mp4"/>
-        </video>
-
-
-        <div className="video-button">
-          <img src={virta} width="50%" alt=""/>
-              <a href={VirtaVideo} target= "_blank" rel="noreferrer">
-              <button type="button">Katso</button>  </a>
-        </div>*/}
   </div>
 
 export default SplashScreen;
